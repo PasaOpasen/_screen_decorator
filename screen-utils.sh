22c3de42ee5cb82a6f78a34ae63dda6eb83fd9ae
Demetry Pascal <qtckpuhdsa@gmail.com>
2024-08-02 15:31:16 +0300

    add dump_screens_output util

--- screen-utils.sh
+++ screen-utils.sh
@@ -3,13 +3,13 @@
 #
 
 function dump_screen_output {
-    name=$1
+    local name=$1
     if [ -z "$name" ]
     then 
         echo "usage: dump_screen_output <screen name or id> <file>"
         return 0
     fi
-    file=${2:-/tmp/screen_output}
+    local file=${2:-/tmp/screen_output}
 
     mkdir -p "$(dirname "$file")"
     if screen -X -S "$name" hardcopy -h $file
@@ -19,5 +19,23 @@ function dump_screen_output {
         echo -e "ERROR: bad screen name $name\n\nexisting screens:"
         screen -ls
     fi
+}
+
+function dump_screens_output {
+    local folder=${1:-/tmp/screen_output.d}
+    if [ -z "$folder" ]
+    then 
+        echo "usage: dump_screens_output <output folder>"
+        return 0
+    fi
+
+    local ident
+    for ident in $(screen -ls | grep -P '^\s+\d+' | awk '{ print $1 }')
+    do 
+        number=${ident%.*}
+        name=${ident#*.}
+
+        dump_screen_output $number "$folder/$name.$number.txt"
+    done
 }
 
